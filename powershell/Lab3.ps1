be46c77657cfb5a90b713164f193192cce91b5a6
Persicjr <122411940+Persicjr@users.noreply.github.com>
2023-01-24 19:51:01 -0500

    Add files via upload
    Lab 3 Script to define $Body

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,15 @@
+﻿function getIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+    }
+
+$IP = getIP
+$USER = $env:UserName
+$Hostname = $env:COMPUTERNAME
+$Version = $HOST.version.Major
+$Date_S = Get-Date -format "dddd, MMMM dd yyyy"
+
+$Body = ("This machine's IP is $IP. User is $User. Hostname is $Hostname. PowerShell Version $Version. Today's date is $Date_S")
+
+Write-Host($Body)
+
+Send-MailMessage -To "persicjr@uc.mail.edu" -From "jakepersic2019@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
